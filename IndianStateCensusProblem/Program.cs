d87fd5291cdcc3facd17860bb14228974761c28a
Diwakar N <diwakar.n@tvsnext.io>
2021-07-28 12:01:21 +0530

    [Diwakar] Initial commit with welcome message

---
+++ IndianStateCensusProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IndianStateCensusProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Initial Commit with welcome message");
+        }
+    }
+}
